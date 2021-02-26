cddcbaa6586b67f0faaf8f45a527e70c1831ccd0
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-26 14:38:51 +0200

    text ship jerky movement along precomputed path

--- game/ships/TextShip.gd
+++ game/ships/TextShip.gd
@@ -6,13 +6,18 @@ Controller for ship with a piece of text in a label in a sprite
 """
 
 export(String) var currentText: String = "test" setget setCurrentText, getCurrentText
+export(float) var speed: float = 450
+export(float) var pathPointIdleSeconds: float = 0.5
+
 
 onready var sprite: Sprite = $Sprite
 onready var label: Label = $Sprite/Label
+onready var pathMover: Tween = $PathMover
 
 
-var path: Array
+var path: Array = []
 var lastPathPointIdx: int = 0
+var remainingPointIdleTime: float = 0.0
 
 
 func _ready():
@@ -20,8 +25,36 @@ func _ready():
 	yield($AnimationPlayer, "animation_finished")
 	path = $PathGenerator.generatePathSegments(position)
 	lastPathPointIdx = 0
-
+	remainingPointIdleTime = pathPointIdleSeconds
 	
+	
+func _process(delta: float):
+	#moving between path points
+	if pathMover.is_active():
+		return
+	
+	#path over
+	if (lastPathPointIdx >= path.size() - 1):
+		return
+	
+	#waiting at a point
+	if remainingPointIdleTime > 0:
+		remainingPointIdleTime -= delta
+		return
+	
+	#wait at idle point over
+	var startPoint = path[lastPathPointIdx]
+	lastPathPointIdx += 1
+	var endPoint = path[lastPathPointIdx]
+	remainingPointIdleTime = pathPointIdleSeconds
+	var moveTime = endPoint.distance_to(startPoint) / speed
+	pathMover.interpolate_property(
+		self, "position", 
+		startPoint, endPoint,  
+		moveTime, Tween.TRANS_SINE, Tween.EASE_OUT
+	)
+	pathMover.start()
+
 
 func setCurrentText(text: String) -> void:
 	currentText = text
