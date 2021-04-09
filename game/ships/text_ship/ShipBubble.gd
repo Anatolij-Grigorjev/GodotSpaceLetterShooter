a4c1562662a87b863141d3c01cc3ae8d5cde5580
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-04-09 10:27:59 +0300

    make bubble burst from shot sometimes

--- game/ships/text_ship/ShipBubble.gd
+++ game/ships/text_ship/ShipBubble.gd
@@ -14,9 +14,15 @@ func _on_Area2D_area_entered(area: Area2D):
 	var areaOwner: Node2D = area.get_parent()
 	if (areaOwner.is_in_group("projectile")):
 		var collideProjectile = areaOwner
-		bubbleHit = true
-		anim.play("hit")
-		yield(anim, "animation_finished")
-		bubbleHit = false
+		if (randi() % 2 == 0):
+			bubbleHit = true
+			anim.play("hit")
+			yield(anim, "animation_finished")
+			bubbleHit = false
+		else:
+			bubbleHit = true
+			anim.play("burst")
+			yield(anim, "animation_finished")
+			bubbleHit = false
 		
 		
