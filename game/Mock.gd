1fd4aa0e42dd84511e67acaca60d2c605d2390a8
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-05-08 09:48:45 +0300

    generalize mock data generation

--- game/Mock.gd
+++ game/Mock.gd
@@ -4,18 +4,22 @@ Test data fixtures for scenes
 """
 
 static func sceneShiledShips(numShips: int) -> SceneSpec:
+	return sceneShipTypeQuantities(0, numShips, 0)
+	
+	
+static func sceneShipTypeQuantities(numFastShips: int, numShieldShips: int, numShooterShips: int) -> SceneSpec:
 	var spec := SceneSpec.new()
 	spec.sceneName = "SPACE"
 	spec.sceneBgColor = Color.cornflower
-	spec.totalShips = numShips
+	spec.totalShips = numFastShips + numShieldShips + numShooterShips
 	spec.smallestShipsWave = 2
 	spec.largestShipsWave = 4
 	spec.allowedShipsTypes = {
 		#speedster
-		SceneShipLimits.new(566, 0, 0): 0,
+		SceneShipLimits.new(566, 0, 0): numFastShips,
 		#tank 
-		SceneShipLimits.new(400.56, 9, 0): numShips,
+		SceneShipLimits.new(400.56, 9, 0): numShieldShips,
 		#shooter
-		SceneShipLimits.new(450.0, 1, 3): 0
+		SceneShipLimits.new(450.0, 1, 3): numShooterShips
 	}
 	return spec
