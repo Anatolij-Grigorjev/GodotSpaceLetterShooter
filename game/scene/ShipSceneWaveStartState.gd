709460c5e068c84c49d86928f516309016fd7e14
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-05-26 17:26:25 +0300

    unroll inner entity methods for fsm state

--- game/scene/ShipSceneWaveStartState.gd
+++ game/scene/ShipSceneWaveStartState.gd
@@ -4,10 +4,24 @@ class_name ShipSceneWaveStartState
 State to perform required animations and startups for next wave
 """
 
+var waveNumber: int = 0
+
 func enterState(prevState: String):
 	.enterState(prevState)
-	entity._prepareNextWaveBGAndTitle()
-	yield(entity._playWaveIntroAnimations(), "completed")
+	_prepareNextWaveBGAndTitle()
+	yield(_playWaveIntroAnimations(), "completed")
+	
+	
+func _prepareNextWaveBGAndTitle():
+	entity.get_node('BG').self_modulate = entity.cachedSpecification.sceneBgColor.darkened(0.25)
+	entity.get_node('CanvasLayer/SceneTitle').text = entity.cachedSpecification.sceneName + ("\nWAVE %02d" % waveNumber)
+	
+	
+func _playWaveIntroAnimations():
+	entity.shooter.anim.play("arrive")
+	yield(entity.shooter.anim, "animation_finished")
+	entity.get_node('AnimationPlayer').play("show_title")
+	yield(entity.get_node('AnimationPlayer'), "animation_finished")
 	
 	
 	
