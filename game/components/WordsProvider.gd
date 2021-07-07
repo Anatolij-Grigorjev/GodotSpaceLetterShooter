5de504de70c179d3b9855092e434fdbe8acad56d
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-07-07 18:35:35 +0300

    more balanced use of words based on frequency

--- game/components/WordsProvider.gd
+++ game/components/WordsProvider.gd
@@ -8,11 +8,19 @@ export(String, FILE, "*.txt") var filePath: String
 
 var words: Array = []
 
+"""
+Array of tuples where item 1 is the word 
+item 2 is useage frequency
+"""
+var wordsWithUseages: Array = []
+
+
 func _ready():
 	randomize()
 	var fileText := _readFileText(filePath)
 	words = fileText.split("\n", false)
 	words.shuffle()
+	wordsWithUseages = _initUnusedWordsArray(words)
 	
 
 func _readFileText(filePath: String) -> String:
@@ -27,6 +35,23 @@ func _readFileText(filePath: String) -> String:
 
 
 func takeWords(numNeeded: int) -> Array:
-	var neededWords := words.slice(0, numNeeded - 1)
-	words.shuffle()
+	var neededWords := []
+	for idx in range(numNeeded):
+		var wordFreqTuple = wordsWithUseages[idx]
+		neededWords.push_back(wordFreqTuple[0])
+		wordFreqTuple[1] += 1
+	wordsWithUseages.sort_custom(self, "_sortByFreqAsc")
+	
 	return neededWords
+	
+	
+
+func _initUnusedWordsArray(wordsArray: Array) -> Array:
+	var result := []
+	for word in wordsArray:
+		result.push_back([word, 0])
+	return result
+	
+	
+func _sortByFreqAsc(tuple1, tuple2) -> bool:
+	return tuple1[1] < tuple2[1]
