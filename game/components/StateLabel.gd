4a4facf3dc46efe80451f06e41e7087a1898ee7d
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-07-18 09:16:50 +0300

    add status label prev state text timeout

--- game/components/StateLabel.gd
+++ game/components/StateLabel.gd
@@ -4,12 +4,28 @@ Label to display latest state transitions of a source FSM
 """
 
 export(NodePath) var sourceFSM: NodePath = ".."
+export(float) var showPrevStateTimeout: float = 1.0
 
 onready var fsm: StateMachine = get_node(sourceFSM)
+onready var timer: Timer = $PrevStateTimeout
+
+
+var prevState: String = StateMachine.NO_STATE
+var currentState: String = StateMachine.NO_STATE
+
 
 func _ready():
 	Utils.tryConnect(fsm, "stateChanged", self, "_onFSMStateChanged")
+	timer.wait_time = showPrevStateTimeout
+	timer.connect("timeout", self, "_onPrevStateDispalyTimeout")
 	
 	
 func _onFSMStateChanged(prevState: String, nextState: String):
-	text = "%s -> %s" % [prevState, nextState]
+	self.prevState = prevState
+	self.currentState = nextState
+	timer.start()
+	text = "%s -> %s" % [self.prevState, self.currentState]
+	
+	
+func _onPrevStateDispalyTimeout():
+	text = self.currentState 
