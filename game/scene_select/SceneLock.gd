1cedea530e9fbbd159010e4896012126ab34866b
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-09-06 19:28:50 +0300

    start work on locked scene panel with points requirement

---
+++ game/scene_select/SceneLock.gd
@@ -0,0 +1,24 @@
+tool
+extends Panel
+"""
+Panel with a lock and points requirement drawn on it
+"""
+export(int) var pointsRequirementAmount: int = 500 setget _setReqAmount
+export(String) var pointsRequirementFormat: String = "unlocks at [%04d] pts." setget _setReqFormat
+
+func _ready():
+	pass
+	
+	
+func _setReqAmount(amount: int):
+	pointsRequirementAmount = amount
+	_updatePointsRequirement()
+	
+	
+func _setReqFormat(format: String):
+	pointsRequirementFormat = format
+	_updatePointsRequirement()
+	
+	
+func _updatePointsRequirement():
+	$VBoxContainer/UnlockPointsRequirement.text = pointsRequirementFormat % pointsRequirementAmount
