7b43b996a8b76ab1d66471054bedb1c2521180d0
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-10-20 18:45:54 +0300

    refactor path generator to not use screen segment concept

--- game/ships/text_ship/PathGenerator.gd
+++ game/ships/text_ship/PathGenerator.gd
@@ -8,46 +8,51 @@ enum Direction {
 	RIGHT = 1
 }
 
-export(int, 2, 100) var widthSegmentsNum: int = 3
-export(int, 100) var minSegmentsBetweenPoints: int = 1
+export(float) var minWidthPrcPerMove: float = 0.1
+export(float) var maxWidthPrcPerMove: float = 0.21
 export(float) var minHeightStep: float =  15.5
 export(float) var maxHeightStep: float = 25.6
-export(Vector2) var pathBoundsHorizMargins: Vector2 = Vector2(50, 50)
+export(float) var pathHorizontalMarginLeft: float = 50
+export(float) var pathHorizontalMarginRight: float = 50
 export(bool) var printDebug: bool = false
 
 var screenHorizBounds: Vector2
 var screenVertBound: float
-var segmentWidth: float
+
+var horizontalMoveLengthBounds: Vector2
 
 
 func _ready():
 	#vars initialized in ready to use editor values
 	screenHorizBounds = Vector2(
-		pathBoundsHorizMargins.x, 
-		OS.window_size.x - pathBoundsHorizMargins.y
+		pathHorizontalMarginLeft, 
+		OS.window_size.x - pathHorizontalMarginRight
 	)
 	screenVertBound = OS.window_size.y
-	segmentWidth = (
-		screenHorizBounds.y - screenHorizBounds.x) / widthSegmentsNum
+	var allowedHorizMoveAreaLength = screenHorizBounds.y - screenHorizBounds.x
+	horizontalMoveLengthBounds = Vector2(
+		allowedHorizMoveAreaLength * minWidthPrcPerMove,
+		allowedHorizMoveAreaLength * maxWidthPrcPerMove
+	)
 		
 	if (printDebug):
 		print("===========[%s.%s]===========" % [owner.name, name])
 		print("Allowed screen horizontal bounds: %s" % screenHorizBounds)
-		print("Allowed screen vertical bounds: %s" % screenVertBound)
-		print("Segments: %s, width/seg: %s" % [widthSegmentsNum, segmentWidth])
-		print("Segements: %s" % [range(screenHorizBounds.x, screenHorizBounds.y + 1, segmentWidth)])
+		print("Allowed screen vertical bounds: %s" % Vector2(0, screenVertBound))
+		print("One horizontal move step range: %s" % horizontalMoveLengthBounds)
+		print("One vertical move step range: %s" % Vector2(minHeightStep, maxHeightStep))
 		print("====================================================")
 		
 
-func generatePathSegments(startPoint: Vector2, minStepDrop: float = minHeightStep, maxStepDtop: float = maxHeightStep) -> Array:
+func generatePathSegments(startPoint: Vector2) -> Array:
 	
 	var position: Vector2 = startPoint
 	var path: Array = [position]
 	while(position.y < screenVertBound):
 		var direction: int = _getNextSegmentDirection(position.x)
 		var moveAmount: Vector2 = Vector2(
-			direction * (minSegmentsBetweenPoints + rand_range(0, 1.0)) * segmentWidth,
-			rand_range(minStepDrop, maxStepDtop)
+			direction * rand_range(horizontalMoveLengthBounds.x, horizontalMoveLengthBounds.y),
+			rand_range(minHeightStep, maxHeightStep)
 		)
 		var nextPosition = position + moveAmount
 		path.push_back(nextPosition)
@@ -65,7 +70,7 @@ func _getNextSegmentDirection(currentX: float) -> int:
 		randomDirection = Direction.LEFT
 	
 	#validate picked direction - must move on-screen
-	var xAfterPathPart = currentX + (randomDirection * segmentWidth * (minSegmentsBetweenPoints + 1))
+	var xAfterPathPart = currentX + (randomDirection * horizontalMoveLengthBounds.y)
 	if (screenHorizBounds.x <= xAfterPathPart and xAfterPathPart <= screenHorizBounds.y):
 		return randomDirection
 	else:
