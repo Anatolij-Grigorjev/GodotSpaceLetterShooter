5dc9c9e0be9c2c484aa02d279cd076d4fd1d69b2
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-12-11 11:47:21 +0200

    change visible spokes to be more visually impactful

--- game/ships/shooter/ShooterShipPreparingState.gd
+++ game/ships/shooter/ShooterShipPreparingState.gd
@@ -14,6 +14,14 @@ func letterTyped(letter: String):
 	_chamberLetter(letter)
 	_addSpoke()
 	
+
+# used for debugging input effects when running without a scene
+#func _input(event: InputEvent) -> void:
+#	if (not event is InputEventKey):
+#		return
+#	var keyCharCode: String = OS.get_scancode_string(event.scancode)
+#	if (keyCharCode.length() == 1):
+#		letterTyped(keyCharCode)
 	
 	
 func _chamberLetter(letter: String):
@@ -24,9 +32,10 @@ func _chamberLetter(letter: String):
 func _addSpoke():
 	var spoke = SpokeScn.instance()
 	spoke.allowedRotationRange = Vector2(150, 210)
-	spoke.scale = Vector2(0.1, 3)
-	spoke.offset = Vector2(0, 50)
+	spoke.scale = Vector2(1, rand_range(1.5, 3))
+	spoke.offset = Vector2(0, rand_range(20, 25))
 	spoke.visible = false
 	
 	entity.sprite.add_child(spoke)
+	spoke.position = entity.shotPosition.position
 	spoke.showSpoke()
