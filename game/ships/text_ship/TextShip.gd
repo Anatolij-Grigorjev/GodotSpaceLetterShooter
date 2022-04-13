67bed32a64579b724a7a9f0ca28b745dfcac506e
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-04-13 18:30:01 +0300

    upgraded to Godot 3.4 and vscode

--- game/ships/text_ship/TextShip.gd
+++ game/ships/text_ship/TextShip.gd
@@ -28,7 +28,7 @@ onready var bubble = $Sprite/ShipBubble
 onready var anim: AnimationPlayer = $AnimationPlayer
 onready var thrusterLeft: Particles2D = $Thrusters/ThrusterLeft
 onready var thrusterRight: Particles2D = $Thrusters/ThrusterRight
-onready var thrustersAudio: AudioStreamPlayer = $Thrusters/AudioStreamPlayer2D
+onready var thrustersAudio: AudioStreamPlayer2D = $Thrusters/AudioStreamPlayer2D
 onready var bodyArea: RigidBody2D = $RigidBody2D
 
 
